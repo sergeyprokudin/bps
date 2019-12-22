06c34ce001c312145fc84ea94099dc473f4732e1
Prokudin <prokus@a483e705f5d5.ant.amazon.com>
2019-12-22 14:01:49 +0100

    readme updated

--- bps_demos/train_modelnet_conv3d.py
+++ bps_demos/train_modelnet_conv3d.py
@@ -175,7 +175,7 @@ def main():
 
     optimizer = pt.optim.Adam(model.parameters(), lr=1e-3)
 
-    n_epochs = 730
+    n_epochs = 800
     pbar = range(0, n_epochs)
     test_accs = []
     test_losses = []
